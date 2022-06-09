743a21e56ab3f71436862735d465c92415eadeb4
Junjae Lee <ejj612o@gmail.com>
2022-06-09 15:17:12 +0900

    회원가입 페이지 추가

--- movie-web/src/routes.js
+++ movie-web/src/routes.js
@@ -1,9 +1,11 @@
 import Main from "./routes/Main.svelte"
 import Login from "./routes/Signin.svelte"
+import SignUp from "./routes/SignUp.svelte"
 
 const routes = {
     "/": Main,
     "/signin": Login,
+    "/signup": SignUp,
 };
 
 export default routes
---
+++ movie-web/src/routes/SignUp.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { Card, Input, Button } from "flowbite-svelte";
+    let id;
+    let password;
+</script>
+
+<Card header="회원가입">
+    <p
+        class="mb-3 font-normal text-gray-700 dark:text-gray-400"
+        slot="paragraph"
+    >
+        <Input
+            class="mb-6"
+            placeholder="example@email.com"
+            label="아이디"
+            bind:value={id}
+        />
+        <Input
+            class="mb-6"
+            placeholder="password"
+            label="비밀번호"
+            type="password"
+            bind:value={password}
+        />
+        <Button
+            type="submit"
+            on:click={() => {
+                console.log(id);
+                console.log(password);
+            }}>제출</Button
+        >
+    </p>
+</Card>
