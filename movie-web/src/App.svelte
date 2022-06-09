a1795943f46a84353b2f1160053100f01434eed7
Junjae Lee <ejj612o@gmail.com>
2022-06-09 13:32:22 +0900

    컴포넌트 분리

--- movie-web/src/App.svelte
+++ movie-web/src/App.svelte
@@ -1,38 +1,20 @@
 <script>
-    import {
-        Button,
-        Navbar,
-        NavBrand,
-        NavHamburger,
-        NavUl,
-        NavLi,
-    } from "flowbite-svelte";
-    import "flowbite/dist/flowbite.css";
+    import Header from "./component/Header.svelte";
     import Router from "svelte-spa-router";
     import routes from "./routes";
+
+    import "flowbite/dist/flowbite.css";
 </script>
 
-<Navbar let:hidden let:toggle>
-    <NavBrand href="/">
-        <img
-            src="https://img.cgv.co.kr/R2014/images/common/logo/logoRed.png"
-            class="mr-3 h-6 sm:h-9"
-            alt="Flowbite Logo"
-        />
-        <span
-            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-        >
-            영화관
-        </span>
-    </NavBrand>
-    <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
-        <NavLi href="/">영화</NavLi>
-        <NavLi href="/">극장</NavLi>
-        <NavLi href="/">로그인</NavLi>
-        <NavLi href="/">회원가입</NavLi>
-    </NavUl>
-</Navbar>
+<Header />
 <main>
     <Router {routes} />
 </main>
+
+<style>
+    main {
+        padding: 1em;
+        max-width: 90%;
+        margin: 1em auto;
+    }
+</style>
---
+++ movie-web/src/component/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+    import {
+        Navbar,
+        NavBrand,
+        NavHamburger,
+        NavUl,
+        NavLi,
+    } from "flowbite-svelte";
+</script>
+
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+        <img
+            src="https://img.cgv.co.kr/R2014/images/common/logo/logoRed.png"
+            class="mr-3 h-6 sm:h-9"
+            alt="Flowbite Logo"
+        />
+        <span
+            class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+        >
+            영화관
+        </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+        <NavLi href="/">영화</NavLi>
+        <NavLi href="/">극장</NavLi>
+        <NavLi href="/">로그인</NavLi>
+        <NavLi href="/">회원가입</NavLi>
+    </NavUl>
+</Navbar>
