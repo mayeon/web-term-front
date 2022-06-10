3c14c17de01bfa76b821c23a654bd86f229181c1
Junjae Lee <ejj612o@gmail.com>
2022-06-10 22:30:56 +0900

    영화 세부정보 페이지 추가

--- movie-web/src/routes.js
+++ movie-web/src/routes.js
@@ -1,13 +1,15 @@
 import Main from "./routes/Main.svelte"
 import Login from "./routes/Signin.svelte"
 import SignUp from "./routes/SignUp.svelte"
+import MovieChart from "./routes/MovieChart.svelte"
 import Movie from "./routes/Movie.svelte"
 
 const routes = {
     "/": Main,
     "/signin": Login,
     "/signup": SignUp,
-    "/movie": Movie,
+    "/movie": MovieChart,
+    "/movie/:movieId": Movie,
 };
 
 export default routes
---
+++ movie-web/src/routes/MovieChart.svelte
@@ -0,0 +1,130 @@
+<script>
+    import { Card } from "flowbite-svelte";
+    import { Rating, Button, Search, Select } from "flowbite-svelte";
+    import { Heart, ThumbUp, EmojiHappy, CurrencyDollar } from "svelte-heros";
+
+    const datas = [
+        {
+            id: 1,
+            image: "https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85689/85689_320.jpg",
+            rate: 3.8,
+            reservation: "78.2",
+            name: "주라기",
+        },
+        {
+            id: 2,
+            image: "https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85689/85689_320.jpg",
+            rate: 3.8,
+            reservation: "78.2",
+            name: "주라기",
+        },
+        {
+            id: 3,
+            image: "https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85689/85689_320.jpg",
+            rate: 3.8,
+            reservation: "78.2",
+            name: "주라기",
+        },
+        {
+            id: 4,
+            image: "https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85689/85689_320.jpg",
+            rate: 3.8,
+            reservation: "78.2",
+            name: "주라기",
+        },
+    ];
+
+    let hoverId = -1;
+    let selected = "normal";
+    let selectprops = {
+        name: "sort type",
+        label: "정렬 기준",
+    };
+</script>
+
+<div class="main-wrap">
+    <h1
+        class="'mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'"
+    >
+        무비차트
+    </h1>
+    <Search />
+    <Select {...selectprops} bind:value={selected}>
+        <option value="normal">기본</option>
+        <option value="select">예매율순</option>
+        <option value="rate">평점순</option>
+    </Select>
+    <div class="cards-wrap">
+        {#each datas as data}
+            <div
+                class="card-wrap"
+                on:mouseenter={() => {
+                    hoverId = data.id;
+                }}
+                on:mouseleave={() => {
+                    hoverId = -1;
+                }}
+            >
+                <Card img={data.image} header={data.name}>
+                    <div slot="paragraph">
+                        <Rating total="5" rating={data.rate}>
+                            <span slot="ratingUp">
+                                <ThumbUp
+                                    class="text-yellow-300 dark:text-yellow-200"
+                                />
+                            </span>
+                            <span slot="ratingDown">
+                                <ThumbUp
+                                    class="text-gray-300 dark:text-gray-500"
+                                />
+                            </span>
+                        </Rating>
+                        <Rating total="5" rating={data.rate}>
+                            <span slot="ratingUp">
+                                <CurrencyDollar
+                                    class="text-red-300 dark:text-red-200"
+                                />
+                            </span>
+                            <span slot="ratingDown">
+                                <CurrencyDollar
+                                    class="text-gray-300 dark:text-gray-500"
+                                />
+                            </span>
+                        </Rating>
+                    </div>
+                    <Button btnColor="red">예매하기</Button>
+                </Card>
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+    .cards-wrap {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        width: 80%;
+        margin: auto;
+    }
+
+    .card-wrap {
+        max-width: 30%;
+        max-height: 30%;
+        width: 20%;
+    }
+
+    .card-wrap > .buttons {
+        display: none;
+        position: relative;
+        margin-top: -20em;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+
+    .card-wrap > .on {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
